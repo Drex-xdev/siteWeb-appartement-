e042161c1579b3dc6ed10e538cd6dc78187c6738
drex_xdev <codingzipdev@gmail.com>
2022-10-15 00:07:57 +0100

    revue js fetch link source image

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,43 @@
-//-----------------------------tout le Selecteur ---------------------------------------
+let table_image = [];
 
+// --------------------------------fetch json ----------------------------------
+fetch("src/json/dataImage.json").then((para1)=> para1.json()).then((para2)=> {
+    // table_image.push(para2[0].link_image)
+    table_image =para2[0].link_image;
+})
+
+// fetch("src/json/dataImage.json").then((pull)=> pull.json()).then((data)=>console.log(data[0].image));
+
+//-----------------------------tout le Selecteur ---------------------------------------
+const img_box = document.getElementsByClassName("img-box");
 
 // --------------------------------tout les Event Listener----------------------------------
 
 
+// --------------------------------code ----------------------------------
+console.log("tab: "+table_image)
+console.log(table_image)
 
-fetch("src/json/dataImage.json").then((pull)=> pull.json()).then((data)=>console.log(data));
+// const lisDesEnfantZoneDajoutDesTache = Array.from( ZoneDajoutDesTache.children);
+// console.log(data[0].link_image)
+/*
+(async ()=>{
+    await fetch('src/json/dataImage.json')
+        .then((para1) => para1.json())
+        .then((para2) => {
+            img =para2[0].image;
+            category=para2[0].category ;
+            titre=para2[0].title;
+            prix=para2[0].price;
+            description=para2[0].description;
+            stock=para2[0].rating.count;
 
+        });
 
-fetch("src/json/dataImage.json").then((pull)=> pull.json()).then((data)=>console.log(data[0].image));
-
-const ZoneDajoutDesTache = document.querySelector(
-    ".art-box"
-);
-
-const ae = document.getElementsByClassName("art-box");
-
-const lisDesEnfantZoneDajoutDesTache = Array.from( ZoneDajoutDesTache.children);
-console.log(lisDesEnfantZoneDajoutDesTache)
+    img_box[0].innerHTML =`<img src="${img}" >`;
+    nonDeLaCategorie[0].textContent= category;
+    titreDeLaCategorie[0].textContent= titre;
+    prixDeLaCategorie[0].textContent= prix;
+    descriptionDeLaCategorie[0].textContent= description;
+    stockDeLaCategorie[0].textContent= stock+" RST";
+})();*/
